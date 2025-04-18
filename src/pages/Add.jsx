d22fddd1e93546e28dd1d38491d6478c8228e0d2
vietnguyen99 <nguyenducviet888999@gmail.com>
2025-04-18 14:14:04 +0700

    hoan than phan admin

---
+++ src/pages/Add.jsx
@@ -0,0 +1,166 @@
+import React, { useState } from "react";
+import { assets } from "../assets/assets";
+import axios from "axios";
+import { backenUrl } from "../App";
+import { toast } from "react-toastify";
+
+const Add = ({ token }) => {
+  const [image1, setImage1] = useState(null);
+  const [image2, setImage2] = useState(null);
+  const [image3, setImage3] = useState(null);
+  const [image4, setImage4] = useState(null);
+
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("Men");
+  const [subCategory, setSubCategory] = useState("Topwear");
+  const [bestseller, setBestseller] = useState(false);
+  const [sizes, setSizes] = useState([]);
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      const formData = new FormData();
+      formData.append("name", name);
+      formData.append("description", description);
+      formData.append("price", price);
+      formData.append("category", category);
+      formData.append("subCategory", subCategory);
+      formData.append("bestseller", bestseller);
+      formData.append("sizes", JSON.stringify(sizes));
+
+      if (image1) formData.append("image1", image1);
+      if (image2) formData.append("image2", image2);
+      if (image3) formData.append("image3", image3);
+      if (image4) formData.append("image4", image4);
+
+      const response = await axios.post(`${backenUrl}/api/product/add`, formData, {
+        headers: { token },
+      });
+     if(response.data.success){
+      toast.success(response.data.message)
+      setName('')
+      setDescription('')
+      setImage1(false)
+      setImage2(false)
+      setImage3(false)
+      setImage4(false)
+      setPrice('')
+     }
+     else{
+      toast.error(response.data.message)
+     }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message)
+    }
+  };
+
+  return (
+    <form onSubmit={onSubmitHandler} className="flex flex-col w-full items-start gap-3">
+      <div>
+        <p className="mb-2">Tải lên hình ảnh</p>
+        <div className="flex gap-2">
+          {[setImage1, setImage2, setImage3, setImage4].map((setImage, index) => (
+            <label key={index} htmlFor={`image${index + 1}`}>
+              <img
+                className="w-20"
+                src={!eval(`image${index + 1}`) ? assets.upload_area : URL.createObjectURL(eval(`image${index + 1}`))}
+                alt=""
+              />
+              <input
+                onChange={(e) => setImage(e.target.files[0])}
+                type="file"
+                id={`image${index + 1}`}
+                hidden
+              />
+            </label>
+          ))}
+        </div>
+      </div>
+
+      <div className="w-full">
+        <p className="mb-2">Tên sản phẩm</p>
+        <input
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+          className="w-full max-w-[500px] px-3 py-2"
+          type="text"
+          placeholder="Type here"
+        />
+      </div>
+
+      <div className="w-full">
+        <p className="mb-2">Mô tả sản phẩm</p>
+        <textarea
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+          className="w-full max-w-[500px] px-3 py-2"
+          placeholder="Write content here"
+        />
+      </div>
+
+      <div className="flex flex-col sm:flex-row gap-2 w-full sm:gap-8">
+        <div>
+          <p className="mb-2">Danh mục sản phẩm</p>
+          <select onChange={(e) => setCategory(e.target.value)} className="w-full px-3 py-2">
+            <option value="Men">Đàn Ông</option>
+            <option value="Women">Phụ Nữ</option>
+            <option value="Kids">Trẻ Em</option>
+          </select>
+        </div>
+
+        <div>
+          <p className="mb-2">Thể loại phụ</p>
+          <select onChange={(e) => setSubCategory(e.target.value)} className="w-full px-3 py-2">
+            <option value="Topwear">Áo Sơ Mi</option>
+            <option value="Bottomwear">Quần Tây</option>
+            <option value="Winterwear">Áo Thun</option>
+          </select>
+        </div>
+
+        <div>
+          <p className="mb-2">Giá sản phẩm</p>
+          <input
+            onChange={(e) => setPrice(e.target.value)}
+            value={price}
+            className="w-full px-3 py-2 sm:w-[120px]"
+            type="number"
+            placeholder="29"
+          />
+        </div>
+      </div>
+
+      <div>
+        <p className="mb-2">Kích thước sản phẩm</p>
+        <div className="flex gap-3">
+          {["S", "M", "L", "XL", "XXL"].map((size) => (
+            <div key={size} onClick={() => setSizes((prev) => prev.includes(size) ? prev.filter(item => item !== size) : [...prev, size])}>
+              <p className={`${sizes.includes(size) ? "bg-pink-100" : "bg-slate-200"} px-3 py-1 cursor-pointer`}>{size}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div>
+        <input
+          onChange={() => setBestseller((prev) => !prev)}
+          checked={bestseller}
+          type="checkbox"
+          id="bestseller"
+        />
+        <label className="cursor-pointer" htmlFor="bestseller">
+          Add to bestseller
+        </label>
+      </div>
+
+      <button type="submit" className="w-28 py-3 mt-4 bg-black text-white">
+        ADD
+      </button>
+    </form>
+  );
+};
+
+export default Add;
